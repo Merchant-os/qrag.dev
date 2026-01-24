d12b38d106236108f9477d11a5cdd08b8d2e5739
merchant-os <merchant-os@users.noreply.github.com>
2026-01-24 21:23:35 +0000

    Add favicon and scroll-to-top on navigation

--- benchmark-ui/src/main.tsx
+++ benchmark-ui/src/main.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Landing } from "./landing/Landing";
 import { AboutPage } from "./pages/AboutPage";
 import { DashboardPage } from "./pages/DashboardPage";
@@ -8,11 +8,22 @@ import { ProcessPage } from "./pages/ProcessPage";
 import { ResultsPage } from "./pages/ResultsPage";
 import "./index.css";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/process" element={<ProcessPage />} />
